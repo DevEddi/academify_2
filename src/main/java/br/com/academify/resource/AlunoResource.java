18ff8c60b877af09fd00af6de1e4fc74eb8eb18a
Edmilson Scovino <scovinowork@gmail.com>
2021-04-18 20:08:57 -0300

    PathVariable

--- src/main/java/br/com/academify/resource/AlunoResource.java
+++ src/main/java/br/com/academify/resource/AlunoResource.java
@@ -5,6 +5,7 @@ import br.com.academify.repository.AlunoReepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,10 @@ public class AlunoResource {
         @GetMapping( "/listar")
         public List<Aluno> listar(){
             return alunoRepository.findAll();
+        }
+
+        @GetMapping("get/{id}")
+        public Aluno get(@PathVariable(value = "id") long id){
+            return alunoRepository.findById(id);
         }
 }
