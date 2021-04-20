7aa2824597f0e82b1e96ffc935c9e79110b8356e
Edmilson Scovino <scovinowork@gmail.com>
2021-04-20 19:40:50 -0300

    Excluir e editar

--- src/main/java/br/com/academify/resource/AlunoResource.java
+++ src/main/java/br/com/academify/resource/AlunoResource.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +31,20 @@ public class AlunoResource {
         @GetMapping("get/{id}")
         public Aluno get(@PathVariable(value = "id") long id){
             return alunoRepository.findById(id);
+        }
+
+        @PostMapping("/incluir")
+        public Aluno incluir(@RequestBody Aluno aluno){
+            return  alunoRepository.save(aluno);
+        }
+        @PutMapping("/editar")
+        public ResponseEntity<Aluno> editar(@RequestBody Aluno aluno){
+                aluno = alunoRepository.save(aluno);
+                return new ResponseEntity<Aluno>(aluno, HttpStatus.OK);
+        }
+
+        public Aluno remover(@RequestBody Aluno aluno){
+                alunoRepository.delete(aluno);
+                return aluno;
         }
 }
