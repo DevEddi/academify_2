173186c0d64967db04cbe75080067fea5286c8eb
Edmilson Scovino <scovinowork@gmail.com>
2021-05-25 23:12:56 -0300

    Jwt

--- academify_front_end/resources/academify.js
+++ academify_front_end/resources/academify.js
@@ -1,17 +1,17 @@
-function GetURLParameter(sParam){
+function GetURLParameter(sParam) {
     var sPageURL = window.location.search.substring(1);
     var sURLVariables = sPageURL.split('&');
-    for(var i = 0; i <sURLVariables.length; i++)
+    for (var i = 0; i < sURLVariables.length; i++)
     {
         var sParameterName = sURLVariables[i].split('=');
-        if(sParameterName[0] == sParam)
+        if (sParameterName[0] == sParam)
         {
             return sParameterName[1];
         }
     }
 }
 
-function esconderAlert(){
+function esconderAlert() {
     $('#div-alert-message').html("<a class='close' onclick='esconderAlert()'>×</a>");
     $('#div-alert-message').hide();
 }
