940136bcd4e2369c14b96a3d5b71cbe95e5df5ee
Edmilson Scovino <scovinowork@gmail.com>
2021-05-26 00:11:15 -0300

    Mais classes para completar jwt, pom com atualizações, authentication 2

--- src/main/java/br/com/academify/model/AuthenticationResponse.java
+++ src/main/java/br/com/academify/model/AuthenticationResponse.java
@@ -1,4 +1,14 @@
 package br.com.academify.model;
 
 public class AuthenticationResponse {
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
 }
